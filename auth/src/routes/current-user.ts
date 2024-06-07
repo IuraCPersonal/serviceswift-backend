125123f0a701b39b26af32886f37a604d33c7546
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-06-07 13:30:18 +0300

    feat(auth): add authentication service

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+import { currentUser } from "@serviceswift/common";
+
+const router = express.Router();
+
+router.get("/api/users/currentUser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+});
+
+export { router as currentUserRouter };
